f945a11a70ac56912eaf684d6cc363ea7ba12459
anyjava <sht21c@gmail.com>
2018-12-03 02:06:56 +0900

    refactoring: @RequestMapping -> @GetMapping 으로 변경

--- spring-cloud/src/main/java/net/anyjava/springboot/study/MockHttpClient.java
+++ spring-cloud/src/main/java/net/anyjava/springboot/study/MockHttpClient.java
@@ -1,9 +1,8 @@
 package net.anyjava.springboot.study;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Mock Api 서비스,
@@ -12,6 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(name = "httpbin", url = "http://httpbin.org")//configuration = FeignConfiguration.class)
 public interface MockHttpClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/delay/{second}")
+    @GetMapping("/delay/{second}")
     String getDelayedResponse(@PathVariable("second") int delaySecond);
 }
