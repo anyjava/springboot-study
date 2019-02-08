c4cc87ce4390d721e94726bb70f4a3f98f1373c1
anyjava <sht21c@gmail.com>
2019-02-08 20:49:45 +0900

    추가 설정값 수정
    - Contract Bean 설정 추가
    - loglevel 조정 방법 추가

--- spring-cloud/src/main/java/net/anyjava/springboot/study/feign/FeignConfiguration.java
+++ spring-cloud/src/main/java/net/anyjava/springboot/study/feign/FeignConfiguration.java
@@ -1,6 +1,9 @@
 package net.anyjava.springboot.study.feign;
 
+import feign.Contract;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -9,4 +12,15 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @Configuration
 public class FeignConfiguration {
+
+    /**
+     * 아래 Bean 설정은 꼭 필수로 해야하는건 아닙니다.
+     * org.springframework.cloud:spring-cloud-starter-openfeign 을 사용하면 org.springframework.cloud.openfeign.support.SpringMvcContract 을 자동으로 Bean 생성을 해줍니다.
+     *
+     * @GetMapping @PostMapping 등을 사용할 수 있음.
+     */
+    @Bean
+    public Contract feignContract() {
+        return new SpringMvcContract();
+    }
 }
