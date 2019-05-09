191807b6315c4611d848b092bf3c2e2b515b3ff2
anyjava <sht21c@gmail.com>
2019-05-10 02:27:32 +0900

    version up & Thread 관련 테스트 추가

--- spring-cloud/src/test/java/net/anyjava/springboot/study/feign/MockHttpClientTest.java
+++ spring-cloud/src/test/java/net/anyjava/springboot/study/feign/MockHttpClientTest.java
@@ -2,18 +2,25 @@ package net.anyjava.springboot.study.feign;
 
 import feign.RetryableException;
 import lombok.extern.slf4j.Slf4j;
+import net.anyjava.springboot.study.StudyApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @Slf4j
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = StudyApplication.class)
 public class MockHttpClientTest {
 
     @Autowired
@@ -39,5 +46,34 @@ public class MockHttpClientTest {
     @Test(expected = IllegalArgumentException.class)
     public void testInternalServerErrors() {
         mockHttpClient.getStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    @Test
+    public void test() {
+        log.info("start");
+
+        int count = Arrays.asList("a", "b", "c", "d").parallelStream()
+                .peek(v -> log.error(">> {}", v))
+                .collect(Collectors.toList()).size();
+
+        log.info("end = {}", count);
+
+        new ForkJoinPool(2).execute(() -> log.info("sdfas"));
+
+        aBean.test();
+
+        assertThat(count).isGreaterThan(0);
+    }
+
+    @Autowired
+    private ABean aBean;
+}
+
+@Slf4j
+@Component
+class ABean {
+    @Async
+    public void test() {
+        log.info("async job");
     }
 }
