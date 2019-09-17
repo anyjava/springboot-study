ad546fa84c287f268c290363e8f52353d63ab618
anyjava <sht21c@gmail.com>
2019-09-18 02:50:47 +0900

    ADD spring-boot-dbunit-starter 적용 & gradle version up

--- spring-boot-test/src/test/java/dev/anyjava/springboot/test/domain/EmployeeRepositoryTest.java
+++ spring-boot-test/src/test/java/dev/anyjava/springboot/test/domain/EmployeeRepositoryTest.java
@@ -1,27 +1,18 @@
 package dev.anyjava.springboot.test.domain;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@TestExecutionListeners(value = { DependencyInjectionTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class,
-        TransactionalTestExecutionListener.class,
-        DbUnitTestExecutionListener.class})
 public class EmployeeRepositoryTest {
 
     private static final Long ANY_EMPLOYEE_ID = 1L;
